928aa22746a302474a673fa3149911a7bd92fc73
Squad 5 <nago.thegloriouswalrus@gmail.com>
2022-09-18 10:09:10 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -9,35 +9,35 @@ Config.MaxDrops = 10 -- amount of locations before being forced to return to sta
 Config.Locations = {
     ["main"] = {
         label = "Truck Shed",
-        coords = vector4(153.68, -3211.88, 5.91, 274.5),
+        coords = vector4(-421.4, 6136.34, 31.88, 41.54),
     },
     ["vehicle"] = {
         label = "Truck Storage",
-        coords = vector4(141.12, -3204.31, 5.85, 267.5),
+        coords = vector4(-424.29, 6128.74, 31.46, 231.02),
     },
     ["stores"] ={
         [1] = {
             name = "ltdgasoline",
-            coords = vector4(-41.07, -1747.91, 29.4, 137.5),
+            coords = vector4(1687.26, 3755.3, 34.33, 316.95),
         },
         [2] = {
-            name = "247supermarket",
-            coords = vector4(31.62, -1315.87, 29.52, 179.5),
+            name = "247supermarket8",
+            coords = vector4(175.04, 6642.94, 31.57, 140.07),
         },
         [3] = {
-            name = "robsliquor",
-            coords = vector4(-1226.48, -907.58, 12.32, 119.5),
+            name = "liquorace1",
+            coords = vector4(1398.06, 3606.8, 34.98, 287.49),
         },
         [4] = {
             name = "ltdgasoline2",
-            coords = vector4(-714.13, -909.13, 19.21, 0.5),
+            coords = vector4(2544.81, 2591.65, 37.96, 292.1),
         },
         [5] = {
             name = "robsliquor2",
-            coords = vector4(-1469.78, -366.72, 40.2, 138.5),
+            coords = vector4(-162.79, 6327.52, 31.59, 47.25),
         },
         [6] = {
-            name = "ltdgasoline3",
+            name = "ltdgasoline1",
             coords = vector4(-1829.15, 791.99, 138.26, 46.5),
         },
         [7] = {
@@ -82,19 +82,19 @@ Config.Locations = {
         },
         [17] = {
             name = "ltdgasoline4",
-            coords = vector4(1155.97, -319.76, 69.2, 17.5),
+            coords = vector4(3453.52, 3745.19, 30.52, 263.97),
         },
         [18] = {
-            name = "robsliquor5",
-            coords = vector4(1119.78, -983.99, 46.29, 287.5),
+            name = "yellowjack",
+            coords = vector4(1986.02, 3053.97, 47.22, 59.5),
         },
         [19] = {
             name = "247supermarket10",
-            coords = vector4(382.13, 326.2, 103.56, 253.5),
+            coords = vector4(1507.17, 1505.07, 115.29, 79.02),
         },
         [20] = {
-            name = "hardware",
-            coords = vector4(89.33, -1745.44, 30.08, 143.5),
+            name = "hardware2",
+            coords = vector4(0.35, 6499.81, 31.5, 139.17),
         },
         [21] = {
             name = "hardware2",
